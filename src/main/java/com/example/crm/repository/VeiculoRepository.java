40c0dbe44028a578be6eb7538dbd75d9d2458571
Lucas Zamparo <60469044+lucaszamparo@users.noreply.github.com>
2023-11-14 16:37:15 -0300

    Add files via upload

---
+++ src/main/java/com/example/crm/repository/VeiculoRepository.java
@@ -0,0 +1,11 @@
+package com.example.crm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.crm.model.Veiculo;
+
+@Repository
+public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
+
+}
